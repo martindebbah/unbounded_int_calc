c53df8812e7f519a3bc391e7953fc21a9cb9cbb5
cherifir <rcherifi992@gmail.com>
2022-04-01 16:17:03 +0200

    en tete des fonctions à implementer

--- unbounded_int.h
+++ unbounded_int.h
@@ -1,11 +1,13 @@
 #include <stddef.h>
 
+
+//Structure d'un chiffre (char pointe vers suivant et precedent)
 typedef struct chiffre {
     struct chiffre *suivant;
     char c;
     struct chiffre *precedent;
 }chiffre;
-
+//Liste doublement chainee pointant vers debut et fin
 typedef struct {
     char signe;
     size_t len;
@@ -13,6 +15,33 @@ typedef struct {
     chiffre *dernier;
 }unbounded_int;
 
+
+/*renvoi la structure int qui represnete la chaine de charactere en argument*/
 unbounded_int string2unbounded_int(const char *e);
 
+
+/*renvoi la structure int qui represnete le long donné en argument*/
+unbounded_int ll2unbounded_int(long long i)
+
+
+/*renvoi la chaine de carctere representant l'entier en argumet*/
 char *unbounded_int2string(unbounded_int i);
+
+
+/*Compare deux entier (renvoi -1,0,1 si a <,==,> à b,respectivement)*/
+int unbounded_int_cmp_unbounded_int(unbounded_it a, unbounded_int b);
+
+
+/*meme chose que la precedente mais compare avec un long ? */
+int unbounded_int_cmp_ll(unbounded_int a, long long b);
+
+
+/*renvoi la representation de la somme des deux entiers*/
+unbounded_int unbounded_int_somme(unbounded_int a, unbounded_int b);
+
+/*renvoi la representation de la difference*/
+unbounded_int unbounded_int_difference( unbounded_int a, unbounded_int b);
+
+
+/*renvoi la representation du produit des deux entiers en parametres*/
+unbounded_int unbounded_int_produit( unbounded_int a, unbounded_int b);
