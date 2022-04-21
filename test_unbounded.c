6e30bc55afdc5651aba088b8c4d31325b38b407d
cherifir <rcherifi992@gmail.com>
2022-04-21 18:26:10 +0200

    diff-pos mais qui marche pas completement soucis avec les pointeurs

--- test_unbounded.c
+++ test_unbounded.c
@@ -4,9 +4,9 @@
 int main(void) {
     // Tests à faire pour chaque fonction !
 
-    unbounded_int a = string2unbounded_int("999");
-    unbounded_int b = string2unbounded_int("9");
-    unbounded_int sum = somme_pos(a, b);
+    unbounded_int a = string2unbounded_int("9999999");
+    unbounded_int b = string2unbounded_int("999");
+    unbounded_int sum = diff_pos(a, b);
     printf("%s\n", unbounded_int2string(sum));
 
     return 0;
